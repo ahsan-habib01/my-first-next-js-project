9b4ede4525be417d7204ad2dbc6e4edf6c482ac3
ahsan.habib01 <ahshapon00@gmail.com>
2025-11-23 10:38:08 +0600

    complete the session

--- src/app/items/[id]/page.jsx
+++ src/app/items/[id]/page.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default async function page({ params }) {
   const { id } = await params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
   return (
     <div>
-      <p>Item details page: {id}</p>
+      <p>Name: {data.name}</p>
+      <p>Email: {data.email}</p>
     </div>
   )
 }
--- src/app/items/page.jsx
+++ src/app/items/page.jsx
@@ -1,14 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
-export default function ItemsPage() {
+export default async function ItemsPage() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <p>this is items page</p>
-      <Link href="items/1">Item 1</Link>
-      <Link href="items/2">Item 2</Link>
-      <Link href="items/3">Item 3</Link>
-      <Link href="items/4">Item 4</Link>
+
+      {
+        data.map(user => <Link key={user.id} href={`/items/${user.id}`}>{ user.name}</Link>)
+      }
     </div>
-  )
+  );
 }
